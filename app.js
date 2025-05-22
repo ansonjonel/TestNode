f31f4f77be330b69c0551c4fed8bfaf14b046538
Anson Rodriguez <ansonjonel@gmail.com>
2025-05-22 17:31:40 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, {'Content-Type': 'text/plain'});
+  res.end('Hola desde GitOps con GitHub Actions!\n');
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Servidor corriendo en puerto ${PORT}`);
+});
