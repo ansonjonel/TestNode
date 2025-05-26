80758fc93bf5df3a874fbde0c81d6513acc1d243
Anson Rodriguez <ansonjonel@gmail.com>
2025-05-26 13:08:24 +0200

    nuevos cambios

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hola Super +O User desde GitOps con GitHub Actions!\n');
+  res.end('Hola Usuario +OTV desde GitOps con GitHub Actions!\n');
 });
 
 const PORT = process.env.PORT || 3000;
